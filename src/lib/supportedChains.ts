0819fa34643fedc260715f0a8bd99450d9db03b9
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-03-13 10:19:53 +0100

    berachain

--- src/lib/supportedChains.ts
+++ src/lib/supportedChains.ts
@@ -1,5 +1,22 @@
 import type { Chain } from "thirdweb";
-import { berachain, ethereum } from "thirdweb/chains";
+import { ethereum } from "thirdweb/chains";
+
+const berachain: Chain = {
+  id: 80094,
+  rpc: "https://80094.rpc.thirdweb.com/",
+  nativeCurrency: {
+    name: "BERA",
+    symbol: "BERA",
+    decimals: 18,
+  },
+  name: "Berachain",
+  blockExplorers: [
+    {
+      name: "Berascan",
+      url: "https://berascan.com"
+    },
+  ],
+};
 
 export const supportedChains: Chain[] = [berachain, ethereum];
 export const defaultSelectedChain = berachain;
